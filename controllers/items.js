6045f8811877e51f3506bbb94c325f23d0942e20
braulio <braulio.escobar@pucp.edu.pe>
2023-02-16 15:17:11 -0500

    controller methods added: get, post, patch

--- controllers/items.js
+++ controllers/items.js
@@ -1,7 +1,28 @@
 const getAllItems = (req, res) => {
-    res.send('all items');
+    res.send('get all items');
 }
+
+const createItem = (req, res) => {
+    res.send(req.body);
+}
+
+const getItem = (req, res) => {
+    res.send('get single item');
+}
+
+const updateItem = (req, res) => {
+    res.send('update item');
+}
+
+const deleteItem = (req, res) => {
+    res.send('item deleted');
+}
+
 
 module.exports = {
     getAllItems,
+    createItem,
+    getItem,
+    updateItem,
+    deleteItem,
 }
--- routes/items.js
+++ routes/items.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {getAllItems} = require('../controllers/items');
+const {getAllItems, createItem, getItem, updateItem, deleteItem} = require('../controllers/items');
 
-router.route('/').get(getAllItems);
+router.route('/').get(getAllItems).post(createItem);
+router.route('/:id').get(getItem).patch(updateItem).delete(deleteItem);
+
 
 module.exports = router;
